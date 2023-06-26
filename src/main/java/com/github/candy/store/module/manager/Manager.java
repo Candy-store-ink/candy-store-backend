44dc6f1898bb7be54cdcbc8c6e81c2489b35a614
Alex1 <alexandersh@wizardsdev.com>
2023-06-26 21:23:35 +0300

    CS-75 initialize project and create entities

---
+++ src/main/java/com/github/candy/store/module/manager/Manager.java
@@ -0,0 +1,41 @@
+package com.github.candy.store.module.manager;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "manager", uniqueConstraints = {
+        @jakarta.persistence.UniqueConstraint(columnNames = "email", name = "UK_ON_MANAGER_EMAIL")
+})
+public class Manager {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "second_name")
+    private String secondName;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "price_coefficient", columnDefinition = "DECIMAL(10,2)")
+    private BigDecimal priceCoefficient;
+
+}
